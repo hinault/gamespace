077939c56d2e59eaa3de3b48a259589e3efb21b1
Hinault <ngdonfack@gmail.com>
2020-11-29 08:35:02 -0500

    Add unit test project

---
+++ Tailspin.SpaceGame.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace Tailspin.SpaceGame.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+
+
+        [Fact]
+        public void Test3()
+        {
+
+        }
+
+
+    }
+}
